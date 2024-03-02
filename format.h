afc85332e4e25f8aacb7bbb7782d4974ca6e61b9
Tyler Matijevich <tylermatijevich@gmail.com>
2024-03-02 17:15:22 -0600

    Add external type declaration for IecString

---
+++ format.h
@@ -0,0 +1,8 @@
+#ifndef IECSTRING_FORMAT_H
+#define IECSTRING_FORMAT_H
+
+#include <UserLog.h>
+typedef UserLogFormatType IecStringFormatType;
+#define IECSTRING_FORMAT_INDEX (USERLOG_FORMAT_INDEX)
+
+#endif /* IECSTRING_FORMAT_H */
