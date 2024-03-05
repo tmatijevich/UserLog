88c6207be19743a54c4a9a7515e23ad1194f0fe3
Tyler Matijevich <tylermatijevich@gmail.com>
2024-03-04 21:21:07 -0600

    Simplify create function

--- Create.c
+++ Create.c
@@ -1,45 +1,41 @@
 /*******************************************************************************
- * File: Create.c
+ * File: create.c
  * Created: 2021-12-05
  * 
  * Authors: 
  *   Tyler Matijevich
  * 
  * License:
- *   This file Create.c is part of the UserLog project 
+ *   This file create.c is part of the UserLog project 
  *   released under the MIT license agreement.
  ******************************************************************************/
 
 #include "Main.h"
 
 /* Create a custom logbook in an Init subroutine */
-int32_t UserLogCreate(char *Name, uint32_t Size) {
-	
-	/* Local variables */
-	static ArEventLogCreate_typ CreateLogbook;
-	int32_t Status;
-	UserLogFormatType Arguments;
-	
-	/* Create */
-	IecStringCopy(CreateLogbook.Name, MIN(USERLOG_LOGBOOK_LENGTH + 1, sizeof(CreateLogbook.Name)), Name);
-	CreateLogbook.Size = Size;
-	CreateLogbook.Persistence = arEVENTLOG_PERSISTENCE_PERSIST;
-	CreateLogbook.Execute = true;
-	do
-		ArEventLogCreate(&CreateLogbook);
-	while(CreateLogbook.Busy);
-	
-	Status = CreateLogbook.StatusID;
-	
-	CreateLogbook.Execute = false;
-	ArEventLogCreate(&CreateLogbook);
-	
-	if(Status && Status != arEVENTLOG_ERR_LOGBOOK_EXISTS) {
-		Arguments.i[0] = Status;
-		IecStringCopy(Arguments.s[0], USERLOG_LOGBOOK_LENGTH + 1, Name);
-		UserLogCustom(USERLOG_USER_LOGBOOK, USERLOG_SEVERITY_ERROR, USERLOG_ERROR_FACILITY, USERLOG_CODE_CREATE, 0, NULL, 
-			"UserLog: ArEventLog error %i. Could not create logbook %s.", &Arguments);
-	}
-	
-	return Status;
+int32_t UserLogCreate(char *Name, uint32_t Size)
+{
+    /* Create */
+    ArEventLogCreate_typ create = {0};
+    IecStringCopy(create.Name, sizeof(create.Name), Name);
+    create.Size = Size;
+    create.Persistence = arEVENTLOG_PERSISTENCE_PERSIST;
+    create.Execute = true;
+    do
+        ArEventLogCreate(&create);
+    while (create.Busy);
+    
+    if (create.StatusID && create.StatusID != arEVENTLOG_ERR_LOGBOOK_EXISTS)
+    {
+        /* Log error */
+        UserLogFormatType log_values = {0};
+        log_values.i[0] = create.StatusID;
+        IecStringCopy(log_values.s[0], sizeof(log_values.s[0]), Name);
+        UserLogCustom(USERLOG_USER_LOGBOOK, USERLOG_SEVERITY_ERROR, 
+                      USERLOG_ERROR_FACILITY, USERLOG_CODE_CREATE, 0, NULL, 
+                      "ArEventLog error %i attempting to create logbook "
+                      "\"%s\" with UserLog", &log_values);
+    }
+
+    return create.StatusID;
 }
