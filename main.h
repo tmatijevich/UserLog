bff78b649034e2b8a0e123b2a07eeb50f57dae70
Tyler Matijevich <tylermatijevich@gmail.com>
2024-03-06 22:07:39 -0600

    Prepare header so library can become a submodule in another library

--- main.h
+++ main.h
@@ -19,8 +19,14 @@ extern "C"
 #endif
 
 /* Headers */
+#ifndef USERLOG_HEADER_EXPLICIT
 #include <UserLog.h>
 #include "IecString/main.h"
+#else
+#include <ArEventLog.h>
+#include <sys_lib.h>
+#include <astime.h>
+#endif
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -30,6 +36,10 @@ extern "C"
 #define MIN(x,y) (((x) < (y)) ? (x) : (y))
 
 /* Constants */
+#ifdef USERLOG_HEADER_EXPLICIT
+#define USERLOG_FACILITY 10U
+#define USERLOG_MESSAGE_LENGTH 120U
+#endif
 #define DATA_MESSAGE_SIZE (USERLOG_MESSAGE_LENGTH + 1U)
 #define DATA_CODED_SIZE (40U + USERLOG_MESSAGE_LENGTH + 2U)
 #define FACILITY_ERROR 330U
@@ -38,9 +48,44 @@ extern "C"
 #define CODE_ERROR_CREATE 10020U
 #define LOGBOOK_USER_NAME "$arlogusr"
 
+/* Types */
+#ifdef USERLOG_HEADER_EXPLICIT
+#ifndef USERLOG_FORMAT_TYPE_EXTERNAL
+#define USERLOG_FORMAT_INDEX 5U
+typedef struct UserLogFormatType
+{
+    uint8_t b[6];
+    double f[6];
+    int32_t i[6];
+    char s[6][81];
+} UserLogFormatType;
+#else
+#include "../format.h"
+#endif
+#endif
+
 /* Global variables */
 extern UserLogSeverityEnum severity_level;
 extern const uint8_t severity_map[];
+
+/* Prototypes */
+#ifdef USERLOG_HEADER_EXPLICIT
+uint32_t UserLogBasic(int32_t Severity, uint16_t Code, char *Message);
+uint32_t UserLogAdvanced(int32_t Severity, uint16_t Code, char *Message, 
+                         struct UserLogFormatType *Values);
+uint32_t UserLogCustom(char *Logbook, int32_t Severity, uint16_t Facility, 
+                       uint16_t Code, ArEventLogRecordIDType Origin, 
+                       char *Object, char *Message, 
+                       struct UserLogFormatType *Values);
+uint32_t UserLogEventText(char *Logbook, int32_t Event, 
+                          ArEventLogRecordIDType Origin, char *Object, 
+                          char *Message, struct UserLogFormatType *Values);
+int32_t UserLogCreate(char *Name, uint32_t Size);
+int32_t UserLogSetSeverityLevel(int32_t Level);
+uint8_t UserLogGetSeverity(int32_t Event);
+uint16_t UserLogGetFacility(int32_t Event);
+uint16_t UserLogGetCode(int32_t Event);
+#endif
 
 #ifdef __cplusplus
 };
